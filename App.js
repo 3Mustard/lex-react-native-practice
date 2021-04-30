0d6a31537d3ac33adbc5e99e7782cab65c638813
Matt <m23.cullen@gmail.com>
2021-04-30 13:46:06 -0400

    solution to Exercise: Slider Component

--- App.js
+++ App.js
@@ -1,46 +1,40 @@
 
 import * as React from 'react';
-import { Text, View, StyleSheet, FlatList } from 'react-native';
+import { Text, View, StyleSheet, Switch } from 'react-native';
 
 export default class App extends React.Component {
-render() {
-  var data = [
-    { framework: 'React JS' },
-    { framework: 'Angular JS' },
-    { framework: 'Ember JS' },
-    { framework: 'Knockout JS' },
-    { framework: 'Polymer JS' },
-    { framework: 'Node JS' },
-    { framework: 'Express JS' },
-  ];
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={data}
-        keyExtractor={(item,index)=> index}
-        renderItem={({ item,index }) => (
-          <View style={styles.itemContainer} key={index.toString()}>
-            <Text>{item.framework}</Text>
-          </View>
-        )}
-      />
-    </View>
-  );
+  state = {
+    showName: false,
+    // displayText: 'OFF',
+  };
+  onSwitchChange(value) {
+    this.setState({
+      showName: !this.state.showName,
+      // displayText: this.state.displayText == 'OFF' ? 'ON' : 'OFF',
+    });
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        { this.state.showName ? (
+          <Text>ON</Text>
+        ) : 
+          <Text>OFF</Text> 
+        }
+        <Switch
+          value={this.state.showName}
+          onValueChange={value => this.onSwitchChange(value)}
+        />
+      </View>
+    );
+  }
 }
-}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 30
-  },
-  itemContainer: {
-    height:100,
-    backgroundColor:'skyblue',
-    alignItems:'center',
-    justifyContent:'center',
-    borderRadius:5,
-    borderColor:'black',
-    borderWidth:1,
-    margin:25
-  },
+    backgroundColor: '#D3D3D3',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
