72cab08c76c8a429fa981996d43e7af15989322e
Matt <m23.cullen@gmail.com>
2021-04-30 13:59:38 -0400

    demo of picker component(depreciated now, still works)

--- App.js
+++ App.js
@@ -1,40 +1,49 @@
-
-import * as React from 'react';
-import { Text, View, StyleSheet, Switch } from 'react-native';
-
-export default class App extends React.Component {
-  state = {
-    showName: false,
-    // displayText: 'OFF',
-  };
-  onSwitchChange(value) {
-    this.setState({
-      showName: !this.state.showName,
-      // displayText: this.state.displayText == 'OFF' ? 'ON' : 'OFF',
-    });
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import {Picker} from '@react-native-community/picker';
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      framework: ''
+    }
   }
+  
   render() {
     return (
       <View style={styles.container}>
-        { this.state.showName ? (
-          <Text>ON</Text>
-        ) : 
-          <Text>OFF</Text> 
-        }
-        <Switch
-          value={this.state.showName}
-          onValueChange={value => this.onSwitchChange(value)}
-        />
+        <Text style={styles.paragraph}>
+          Select the JS library/framework to learn
+        </Text>
+        <Picker
+          style={styles.picker}
+          selectedValue={this.state.framework}
+          onValueChange={(fw) => this.setState({framework: fw})}>
+          <Picker.Item label="Angular" value="Angular" />
+          <Picker.Item label="React JS" value="React JS" />
+          <Picker.Item label="Ember JS" value="Ember JS" />
+          <Picker.Item label="Vue JS" value="Vue JS" />
+        </Picker>
+        {this.state.framework!=''?<Text>You selected {this.state.framework}</Text>:<Text>You selected </Text>}
       </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#D3D3D3',
+    alignItems: 'center',
     justifyContent: 'center',
-    alignItems: 'center'
+    paddingTop: 30
+  },
+  paragraph: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  picker: {
+    width: 200,
+    borderColor: 'black',
+    borderWidth: 3,
   }
 });
